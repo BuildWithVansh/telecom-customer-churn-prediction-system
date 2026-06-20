608a4f46cf09e7123d24467bf9e6b41f05485e6a
Vansh Gupta <vanshguptaa172@gmail.com>
2026-06-21 01:25:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,120 @@
+
+import streamlit as st
+import pandas as pd
+import joblib
+
+# Load Model
+model = joblib.load("customer_churn_pipeline.pkl")
+
+st.title("📊 Customer Churn Prediction")
+
+gender = st.selectbox("Gender", ["Male", "Female"])
+SeniorCitizen = st.selectbox("Senior Citizen", [0, 1])
+Partner = st.selectbox("Partner", ["Yes", "No"])
+Dependents = st.selectbox("Dependents", ["Yes", "No"])
+
+tenure = st.number_input("Tenure", min_value=0)
+
+PhoneService = st.selectbox("Phone Service", ["Yes", "No"])
+MultipleLines = st.selectbox("Multiple Lines", ["Yes", "No", "No phone service"])
+
+InternetService = st.selectbox(
+    "Internet Service",
+    ["DSL", "Fiber optic", "No"]
+)
+
+OnlineSecurity = st.selectbox(
+    "Online Security",
+    ["Yes", "No", "No internet service"]
+)
+
+OnlineBackup = st.selectbox(
+    "Online Backup",
+    ["Yes", "No", "No internet service"]
+)
+
+DeviceProtection = st.selectbox(
+    "Device Protection",
+    ["Yes", "No", "No internet service"]
+)
+
+TechSupport = st.selectbox(
+    "Tech Support",
+    ["Yes", "No", "No internet service"]
+)
+
+StreamingTV = st.selectbox(
+    "Streaming TV",
+    ["Yes", "No", "No internet service"]
+)
+
+StreamingMovies = st.selectbox(
+    "Streaming Movies",
+    ["Yes", "No", "No internet service"]
+)
+
+Contract = st.selectbox(
+    "Contract",
+    ["Month-to-month", "One year", "Two year"]
+)
+
+PaperlessBilling = st.selectbox(
+    "Paperless Billing",
+    ["Yes", "No"]
+)
+
+PaymentMethod = st.selectbox(
+    "Payment Method",
+    [
+        "Electronic check",
+        "Mailed check",
+        "Bank transfer (automatic)",
+        "Credit card (automatic)"
+    ]
+)
+
+MonthlyCharges = st.number_input(
+    "Monthly Charges",
+    min_value=0.0
+)
+
+TotalCharges = st.number_input(
+    "Total Charges",
+    min_value=0.0
+)
+
+if st.button("Predict Churn"):
+
+    data = pd.DataFrame({
+        "gender": [gender],
+        "SeniorCitizen": [SeniorCitizen],
+        "Partner": [Partner],
+        "Dependents": [Dependents],
+        "tenure": [tenure],
+        "PhoneService": [PhoneService],
+        "MultipleLines": [MultipleLines],
+        "InternetService": [InternetService],
+        "OnlineSecurity": [OnlineSecurity],
+        "OnlineBackup": [OnlineBackup],
+        "DeviceProtection": [DeviceProtection],
+        "TechSupport": [TechSupport],
+        "StreamingTV": [StreamingTV],
+        "StreamingMovies": [StreamingMovies],
+        "Contract": [Contract],
+        "PaperlessBilling": [PaperlessBilling],
+        "PaymentMethod": [PaymentMethod],
+        "MonthlyCharges": [MonthlyCharges],
+        "TotalCharges": [TotalCharges]
+    })
+
+    prediction = model.predict(data)[0]
+    probability = model.predict_proba(data)[0][1]
+
+    if prediction == 1:
+        st.error(
+            f"⚠️ Customer Likely to Churn\n\nProbability: {probability:.2%}"
+        )
+    else:
+        st.success(
+            f"✅ Customer Likely to Stay\n\nProbability of Churn: {probability:.2%}"
+        )
